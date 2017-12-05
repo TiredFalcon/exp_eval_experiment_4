868a104e887a2b40585015a69502ad5820cb0444
falcon <aron.fiechter@gmail.com>
2017-12-05 22:14:34 +0100

    Add very small Main class to test

---
+++ constants_vs_literals/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by falcon on 05.12.17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Started");
+
+        System.out.println(ApproximationC.e());
+    }
+}
