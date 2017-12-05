7b0885a6cbcb029c114d0df20ca6d464be081a68
falcon <aron.fiechter@gmail.com>
2017-12-05 23:53:34 +0100

    Add some test in Main

--- constants_vs_literals/src/Main.java
+++ constants_vs_literals/src/Main.java
@@ -5,7 +5,21 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Started");
+        System.out.println();
 
-        System.out.println(ApproximationC.e());
+        System.out.println("Running BankAccount");
+        System.out.println("create new account with 1000.0");
+        BankAccountC account = new BankAccountC("falcon", 1000.0);
+        System.out.println("withdraw 100: " + account.withdraw(100.0));
+        System.out.println("withdraw 1000: " + account.withdraw(1000.0));
+        System.out.println("deposit 700: " + account.deposit(700.0));
+        account.annualUpdate();
+        System.out.println();
+
+
+        System.out.println("Running Approximation");
+        System.out.println("sin(pi/4) = " + ApproximationC.sin(ApproximationC.PI / 4.0));
+        System.out.println("e = " + ApproximationC.e());
+        System.out.println();
     }
 }
