de3c1edc7f9310363ea7b800e305aa2d0da4c66e
falcon <aron.fiechter@gmail.com>
2017-12-05 15:23:40 +0100

    BankAccountC class added
    Committ everything because I don't care.

---
+++ constants_vs_literals/src/BankAccountC.java
@@ -0,0 +1,35 @@
+/**
+ * Provide basic API for operations on a bank account
+ *
+ * @author falcon
+ * @version 2017-12-05
+ */
+public class BankAccountC {
+
+    private static final double MONTHLY_MAX_WITHDRAWAL = 3000.0;
+    private static final double MONTHLY_INTEREST_RATE = 0.002;
+    private static final double ANNUAL_ACCOUNT_COST = 25.0;
+
+    // instance variables
+    private int totalAmount;
+    private int withdrawalsThisMonth;
+
+    /**
+     * Constructor for objects of class BankAccountC
+     */
+    public BankAccountC() {
+        // initialise instance variables
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return y;
+    }
+}
